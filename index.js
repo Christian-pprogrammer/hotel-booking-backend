e66e1e079398468eb957101b3600e5b653641102
MPANO Christian <mpanoc6@gmail.com>
2022-02-02 22:08:53 +0200

    added order routes file

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const dotenv = require('dotenv');
+dotenv.config({
+    path: './.env'
+})
+const app = express();
+
+const PORT = process.env.PORT || 6000;
+app.listen(PORT, () => {
+    console.log(`app listening on port ${PORT}`);
+})
