8da5b4bbb545539ee8a88dd49b092f0a61a12ca6
MPANO Christian <mpanoc6@gmail.com>
2022-02-03 22:10:44 +0200

    added User model

---
+++ models/User.js
@@ -0,0 +1,30 @@
+const mongoose = require('mongoose');
+const userSchema = new mongoose.Schema({
+    firstName: {
+        type: String,
+        required: true
+    },
+    lastName: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true
+    },
+    telephone: {
+        type: String,
+        required: true
+    },
+    username: {
+        type: String,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    }
+})
+
+const User = mongoose.model("User", userSchema);
+module.exports.User = User;
