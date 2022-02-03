ed650e0effacac1b32b18ffec8013c307e3d2008
MPANO Christian <mpanoc6@gmail.com>
2022-02-03 22:05:04 +0200

    added routing for user registration

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
+const passport = require('passport');
 const express = require('express');
 const dotenv = require('dotenv');
 dotenv.config({
     path: './.env'
 })
 const app = express();
-
+app.use(passport.initialize());
+require('./config/passport.config')(passport);
+app.use('/')
 const PORT = process.env.PORT || 6000;
 app.listen(PORT, () => {
     console.log(`app listening on port ${PORT}`);
---
+++ routes/userRoutes.js
@@ -0,0 +1,7 @@
+const express = require('express');
+const router = express.Router();
+const { createUser } = require('../controllers/userController');
+router.route('/register')
+    .post(createUser)
+
+module.exports = router;
