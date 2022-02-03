a98ddfc39a1dc84b04d229887fd6f9ef1bf60fda
MPANO Christian <mpanoc6@gmail.com>
2022-02-03 22:25:37 +0200

    added register user

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,6 +1,11 @@
+const { User } = require('../controllers/userController');
 exports.createUser = async (req,res) => {
     try {
-        
+        const created = await User.create(req.body);
+        return res.status(200).json({
+          status: true,
+          user: created,
+        });
     }catch(err) {
         res.status(400).json({
             status: false,
