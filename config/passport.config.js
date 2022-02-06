1b3644a8b0f5a10be4009f1e861c184ecf9c2c08
MPANO Christian <mpanoc6@gmail.com>
2022-02-06 20:00:00 +0200

    added passport middleware

--- config/passport.config.js
+++ config/passport.config.js
@@ -1,3 +1,16 @@
+const JwtStrategy = require('passport-jwt').Strategy;
+const ExtractJwt = require('passport-jwt').ExtractJwt;
+const { User } = require('../models/User');
 module.exports = (passport) => {
-    
+    passport.use(new JwtStrategy({
+        jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+        secretOrKey: process.env.JWT_PRIVATE_KEY
+    }), async (payload, done) => {
+        const validUser = await User.findById(payload.id);
+        if(validUser) {
+            return done(null, validUser);
+        }else{
+            return done(null, false);
+        }
+    })
 }
