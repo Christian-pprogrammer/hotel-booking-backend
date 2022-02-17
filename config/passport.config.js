a849bc68d962aa6f5d868b18c135b54184220b76
Christian-pprogrammer <mpanoc6@gmail.com>
2022-02-17 20:12:59 +0200

    resolved an error in passport config

--- config/passport.config.js
+++ config/passport.config.js
@@ -3,14 +3,14 @@ const ExtractJwt = require('passport-jwt').ExtractJwt;
 const { User } = require('../models/User');
 module.exports = (passport) => {
     passport.use(new JwtStrategy({
-        jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+        jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken,
         secretOrKey: process.env.JWT_PRIVATE_KEY
-    }), async (payload, done) => {
+    }, async (payload, done) => {
         const validUser = await User.findById(payload.id);
         if(validUser) {
             return done(null, validUser);
         }else{
             return done(null, false);
         }
-    })
+    }))
 }
