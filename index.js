8e3701079e3f9071acc90e0c1013888f3518e435
Christian-pprogrammer <mpanoc6@gmail.com>
2022-02-19 18:43:00 +0200

    added routing for rooms

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ const userRouter = require('./routes/userRoutes');
 const roomRouter = require('./routes/roomRoutes');
 const hallRouter = require('./routes/hallRoutes');
 const orderRouter = require('./routes/orderRoutes');
+const { Room } = require('./models/Room');
 connect();
 const app = express();
 app.use(express.json());
@@ -17,6 +18,7 @@ app.use(cors());
 app.use(passport.initialize());
 require('./config/passport.config')(passport);
 app.use('/users', userRouter);
+app.use('/rooms', roomRouter);
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
     console.log(`app listening on port ${PORT}`);
