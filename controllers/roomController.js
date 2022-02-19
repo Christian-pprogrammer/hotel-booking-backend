8038ad6e254bff79d1b327831f1179b82f33643b
Christian-pprogrammer <mpanoc6@gmail.com>
2022-02-19 17:50:19 +0200

    added get all rooms

--- controllers/roomController.js
+++ controllers/roomController.js
@@ -13,3 +13,18 @@ exports.createRoom = async (req,res) => {
         })
     }
 }
+
+exports.getAllRooms = async (req,res) => {
+    try{
+        const rooms = await Room.find();
+        res.status(200).json({
+            success: true,
+            rooms: rooms
+        })
+    }catch(err) {
+        res.status(400).json({
+            success: false,
+            message: err.message
+        })
+    }
+}
