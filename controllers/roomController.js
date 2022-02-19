0fb44761cb6efeb5a6a8debaa59caf6951c97e57
Christian-pprogrammer <mpanoc6@gmail.com>
2022-02-19 17:49:06 +0200

    added create room

--- controllers/roomController.js
+++ controllers/roomController.js
@@ -1,7 +1,15 @@
+const { Room } = require("../models/Room");
 exports.createRoom = async (req,res) => {
     try {
-
+        const created = await Room.create(req.body);
+        res.status(200).json({
+            success: true,
+            room: created
+        })
     }catch(err) {
-        
+        res.status(400).json({
+            success: false,
+            message: err.message
+        })
     }
 }
